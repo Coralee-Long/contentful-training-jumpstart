8e285be5b6c54137b8df65a2d541dc26f98c521b
Coralee <coralee.long.partner@decathlon.com>
2024-08-12 09:23:40 +0200

    add LivePreview setup

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,42 @@
-import _ from "lodash";
-import ProductSection from "../components/ProductSection";
-import { getEntriesByContentType } from "../lib/helpers";
+import _ from 'lodash';
+import ProductSection from '../components/ProductSection';
+import { getEntriesByContentType } from '../lib/helpers';
+import {
+  useContentfulLiveUpdates,
+  useContentfulInspectorMode,
+} from '@contentful/live-preview/react';
+
+const DEFAULT_LOCALE = 'en-US';
 
 export default function Home(props) {
-  const page = _.get(props, "page");
-  const sections = _.get(page, "fields.sections"); // this field is an array of page sections
-  const headline = _.get(page, "fields.headline");
+  const page = _.get(props, 'page');
+  const updatedPage = useContentfulLiveUpdates(page, DEFAULT_LOCALE) || page;
+  const sections = _.get(updatedPage, 'fields.sections'); // this field is an array of page sections
+  const headline = _.get(updatedPage, 'fields.headline');
 
-  console.log(page); // you can view this object in the console
+  const inspectorProps = useContentfulInspectorMode();
 
+  console.log(updatedPage); // you can view this object in the console
 
   return (
     <>
       {/* {JSON.stringify(page)} */}
-      <h1 className="font-bold text-2xl mb-4 text-center">{headline}</h1>
+      <h1
+        {...inspectorProps({
+          entryId: page.sys.id,
+          fieldId: 'headline',
+        })}
+        className="font-bold text-2xl mb-4 text-center"
+      >
+        {headline}
+      </h1>
       <div className="flex flex-col space-y-4">
         {Array.isArray(sections)
           ? sections.map((section, sectionIndex) => {
-              const contentType = _.get(section, "sys.contentType.sys.id");
-              const sectionId = _.get(section, "sys.id");
-              const fields = _.get(section, "fields");
-              if (contentType === "productSection") {
+              const contentType = _.get(section, 'sys.contentType.sys.id');
+              const sectionId = _.get(section, 'sys.id');
+              const fields = _.get(section, 'fields');
+              if (contentType === 'productSection') {
                 return (
                   <ProductSection
                     key={sectionId}
@@ -30,21 +46,21 @@ export default function Home(props) {
                 );
               }
 
-              if (contentType === "product") {
+              if (contentType === 'product') {
                 return <>{contentType}</>;
               }
 
-              return "";
+              return '';
             })
-          : ""}
+          : ''}
       </div>
     </>
   );
 }
 
 export async function getStaticProps() {
-  const pageEntries = await getEntriesByContentType("landingPage", "home-page");
-  let homepageEntry = _.get(pageEntries, "items[0]");
+  const pageEntries = await getEntriesByContentType('landingPage', 'home-page');
+  let homepageEntry = _.get(pageEntries, 'items[0]');
 
   return {
     props: {
